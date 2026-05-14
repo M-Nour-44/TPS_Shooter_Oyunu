0c70f234e92419045a4094a90c252600f3145fe6
M-Nour-44 <mhmdnormasre99@gmail.com>
2026-05-15 02:14:21 +0300

    Rifle Effects

---
+++ Assets/Scripts/Rifle.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rifle : MonoBehaviour
+{
+    [Header("Rifle Things")]
+    public Camera camera;
+    public float giveDamageOf = 10f;
+    public float shootingRange = 100f;
+    public float fireCharge = 15f;
+    public Animator animator;
+    public PlayerScript player;
+
+    [Header("Rifle Ammunition and shooting")]
+    private int maximumAmmunition = 20;
+    private int mag = 15;
+    private int presentAmmunition;
+    public float reloadingTime = 1.3f;
+    private bool setReloading = false;
+    private float nextTimeToShoot = 0f;
+
+    [Header("Rifle Effects")]
+    public ParticleSystem muzzleSpark;
+    public GameObject impactEffect;
+
+    //  [Header("Sounds and UI")]
+
+    private void Awake()
+    {
+        presentAmmunition = maximumAmmunition;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (setReloading)
+            return;
+        if(presentAmmunition <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToShoot)
+        {
+            animator.SetBool("Fire", true);
+            animator.SetBool("Idle", false);
+            nextTimeToShoot = Time.time + 1f / fireCharge;
+            shoot();
+        }
+        else if(Input.GetButton("Fire1") && Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            animator.SetBool("Idle", false);
+            animator.SetBool("IdleAim", true);
+            animator.SetBool("FireWalk", true);
+            animator.SetBool("Walk", true);
+            animator.SetBool("Reloading", false);
+        }
+        else if (Input.GetButton("Fire2") && Input.GetButton("Fire1"))
+        {
+            animator.SetBool("Idle", false);
+            animator.SetBool("IdleAim", true);
+            animator.SetBool("FireWalk", true);
+            animator.SetBool("Walk", true);
+            animator.SetBool("Reloading", false);
+        }
+        else
+        {
+            animator.SetBool("Fire", false);
+            animator.SetBool("Idle", true);
+            animator.SetBool("FireWalk", false);
+            animator.SetBool("Reloading", false);
+        }
+    }
+    void shoot()
+    {
+        //Check for mag
+        if(mag == 0)
+        {
+            //Show ammo out text
+            return;
+        }
+
+        presentAmmunition--;
+
+        if(presentAmmunition == 0)
+        {
+            mag--;
+        }
+
+        muzzleSpark.Play();
+        RaycastHit hitInfo;
+
+        if(Physics.Raycast(camera.transform.position,camera.transform.forward, out hitInfo, shootingRange))
+        {
+            Debug.Log(hitInfo.transform.name);
+
+            Objects objects = hitInfo.transform.GetComponent<Objects>();
+
+            if(objects != null )
+            {
+                objects.objectHitDamage(giveDamageOf);
+                GameObject impactGo = Instantiate(impactEffect, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
+                Destroy(impactGo, 1f);
+            }
+            
+        }
+    }
+    IEnumerator Reload()
+    {
+        player.playerSpeed = 0f;
+        player.playerSprint = 0f;
+        setReloading = true;
+        Debug.Log("Reloading....");
+        animator.SetBool("Reloading", true);
+        yield return new WaitForSeconds(reloadingTime);
+        animator.SetBool("Reloading", false);
+        presentAmmunition = maximumAmmunition;
+        player.playerSpeed = 1.9f;
+        player.playerSprint = 3f;
+        setReloading = false;
+    }
+}
--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -6,6 +6,7 @@ public class PlayerScript : MonoBehaviour
 {
     [Header("Player Movement")]
     public float playerSpeed = 1.9f;
+    public float playerSprint = 3f;
 
     [Header("Player Script Cameras")]
     public Transform PlayerCamera;
@@ -13,6 +14,7 @@ public class PlayerScript : MonoBehaviour
     [Header("Player Animator and Gravity")]
     public CharacterController cC;
     public float gravity = -9.81f;
+    public Animator animator;
     
    // [Header("Player Health things")]
 
@@ -29,7 +31,7 @@ public class PlayerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-            
+            Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -48,6 +50,8 @@ public class PlayerScript : MonoBehaviour
         playerMove();
 
         Jump();
+
+        Sprint();
     }
 
     void playerMove()
@@ -59,6 +63,13 @@ public class PlayerScript : MonoBehaviour
 
         if(direction.magnitude >= 0.1f)
         {
+            animator.SetBool("Walk", true);
+            animator.SetBool("Running", false);
+            animator.SetBool("Idle", false);
+            animator.SetTrigger("Jump");
+            animator.SetBool("AimWalk", false);
+            animator.SetBool("IdleAim", false);
+
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + PlayerCamera.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnCalmVelocity, turnCalmTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
@@ -66,13 +77,60 @@ public class PlayerScript : MonoBehaviour
             Vector3 MoveDirection = Quaternion.Euler(0f , targetAngle , 0f) * Vector3.forward;
             cC.Move(MoveDirection.normalized * playerSpeed * Time.deltaTime);
         }
+        else
+        {
+            animator.SetBool("Idle", true);
+            animator.SetTrigger("Jump");
+            animator.SetBool("Walk", false);
+            animator.SetBool("Running", false);
+            animator.SetBool("AimWalk", false);
+        }
     }
 
     void Jump()
     {
         if(Input.GetButtonDown("Jump") && onSurface)
         {
-        velocity.y = Mathf.Sqrt(jumpRange * -2 * gravity);
+            animator.SetBool("Walk", false);
+            animator.SetTrigger("Jump");
+
+            velocity.y = Mathf.Sqrt(jumpRange * -2 * gravity);
         }
+        else
+        {
+            animator.ResetTrigger("Jump");
+
+        }
+    }
+    void Sprint()
+    {
+        if (Input.GetButton("Sprint") && Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) && onSurface)
+        {
+            float horizontal_axis = Input.GetAxisRaw("Horizontal");
+            float vertical_axis = Input.GetAxisRaw("Vertical");
+
+            Vector3 direction = new Vector3(horizontal_axis, 0f, vertical_axis).normalized;
+
+            if (direction.magnitude >= 0.1f)
+            {
+                animator.SetBool("Running", true);
+                animator.SetBool("Idle", false);
+                animator.SetBool("Walk", false);
+                animator.SetBool("IdleAim", false);
+
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + PlayerCamera.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnCalmVelocity, turnCalmTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                Vector3 MoveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                cC.Move(MoveDirection.normalized * playerSprint * Time.deltaTime);
+            }
+            else
+            {
+                animator.SetBool("Idle", false);
+                animator.SetBool("Walk", false);
+            }
+        }
+
     }
 }
