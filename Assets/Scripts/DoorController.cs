6642a0b69b136a8edbf6fc3f8de1acb924d28acc
abodbeek <abdo.q.2002@gmail.com>
2026-05-18 04:42:39 +0300

    HealthBar & Sound Effects added

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -2,13 +2,21 @@ using UnityEngine;
 
 public class DoorController : MonoBehaviour
 {
+
     public Animator animator;
 
     private bool isOpen = false;
 
+    [Header("Sound Efeect")]
+    public AudioClip gateSound;
+    public AudioSource audioSource;
+ 
     public void ToggleDoor()
     {
         isOpen = !isOpen;
         animator.SetBool("IsOpen", isOpen);
+        audioSource.PlayOneShot(gateSound);
     }
+
+    
 }
